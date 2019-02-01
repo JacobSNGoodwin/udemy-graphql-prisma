ca6b7796087c39db6bf9e405b4c9043ba80bcd61
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-01 15:10:58 -0800

    Lecture 59 - Use prisma to get users and posts in grpahql queries

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,23 +1,10 @@
-const Query = {
-    users(parent, args, { db }, info) {
-        if (!args.query) {
-            return db.users
-        }
-
-        return db.users.filter((user) => {
-            return user.name.toLowerCase().includes(args.query.toLowerCase())
-        })
+ const Query = {
+    users(parent, args, { prisma }, info) {
+        // info provides info or requst coming from clients
+        return prisma.query.users(null, info)
     },
-    posts(parent, args, { db }, info) {
-        if (!args.query) {
-            return db.posts
-        }
-
-        return db.posts.filter((post) => {
-            const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
-            const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
-            return isTitleMatch || isBodyMatch
-        })
+    posts(parent, args, { prisma }, info) {
+        return prisma.query.posts(null, info)
     },
     comments(parent, args, { db }, info) {
         return db.comments
