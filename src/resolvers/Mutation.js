43165e50421609b5b3ff7077c93a1ead6a097bf5
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-02 09:53:26 -0800

    Add password when creating user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,14 +1,19 @@
-import uuidv4 from 'uuid/v4'
+import bcrypt from 'bcryptjs'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
-        // const emailTaken = await prisma.exists.User({ email: args.data.email })
+        if (args.data.password.length < 8) {
+            throw new Error('Password must be 8 characters or longer')
+        }
 
-        // if (emailTaken) {
-        //     throw new Error('Email already in use ')
-        // }
+        const password = await bcrypt.hash(args.data.password, 10)
 
-        return prisma.mutation.createUser({ data: args.data }, info)
+        return prisma.mutation.createUser({ 
+            data: {
+                ...args.data,
+                password: password
+            }
+        }, info)
     },
     async deleteUser(parent, args, { prisma }, info) {
         // const userExists = await prisma.exists.User({ id: args.id })
