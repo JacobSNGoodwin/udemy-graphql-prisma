d79ba1f8010b7d94281a5ff60e282340f629594d
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-02 23:01:44 -0800

    Lecture 69 - Create jwt when creating user

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,19 +1,25 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 
 const Mutation = {
     async createUser(parent, args, { prisma }, info) {
         if (args.data.password.length < 8) {
             throw new Error('Password must be 8 characters or longer')
         }
-
+        
         const password = await bcrypt.hash(args.data.password, 10)
 
-        return prisma.mutation.createUser({ 
+        const user = await prisma.mutation.createUser({ 
             data: {
                 ...args.data,
                 password: password
             }
         }, info)
+
+        return {
+            user,
+            token: jwt.sign({ userId: user.id }, 'thisisasecret')
+        }
     },
     async deleteUser(parent, args, { prisma }, info) {
         // const userExists = await prisma.exists.User({ id: args.id })
