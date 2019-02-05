dddb8f7cc7e6ad3b43d05393f2ad16c750df93b6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-05 11:40:32 -0800

    Lecture 77 - Implement fragments

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,14 +1,32 @@
 import getUserId from '../utils/getUserId'
 
 const User = {
-  email(parent, args, { request }, info) {
-    // if not authenticated, get null back
-    const userId = getUserId(request, false)
-
-    if (userId && userId === parent.id) {
-      return parent.email
-    } else {
-      return null
+  // use fragment to make sure we get the id, so that way we can get email even if id isn't queried
+  email: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { request }, info) {
+      // if not authenticated, get null back
+      const userId = getUserId(request, false)
+  
+      if (userId && userId === parent.id) {
+        return parent.email
+      } else {
+        return null
+      }
+    }
+  },
+  posts: {
+    fragment: 'fragment userId on User { id }',
+    resolve(parent, args, { prisma }, info) {
+      // if not authenticated, get null back
+      return prisma.query.posts({
+        where: {
+          published: true,
+          author: {
+            id: parent.id
+          }
+        }
+      })
     }
   }
 }
