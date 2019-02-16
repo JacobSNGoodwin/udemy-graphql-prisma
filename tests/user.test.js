01dd54d9b7e61516be63bf0524ea82185dd76dd6
Jacob Goodwin <jacob.goodwin@gmail.com>
2019-02-15 17:03:16 -0800

    Add tests for posts and users queries

--- tests/user.test.js
+++ tests/user.test.js
@@ -73,3 +73,43 @@ test('Should create a new user', async () => {
 
   expect(userExists).toBe(true)
 })
+
+test('Should expose public author profiles', async () => {
+  const getUsers = gql`
+    query {
+      users {
+        id
+        name
+        email
+      }
+    }
+  `
+
+  const response = await client.query({
+    query: getUsers
+  })
+
+  expect(response.data.users.length).toBe(1)
+  expect(response.data.users[0].email).toBe(null)
+  expect(response.data.users[0].name).toBe('Bob')
+})
+
+test('Should expose public posts', async () => {
+  const getPosts = gql`
+    query {
+      posts {
+        id
+        body
+        title
+        published
+      }
+    }
+  `
+
+  const response = await client.query({
+    query: getPosts
+  })
+
+  expect(response.data.posts.length).toBe(1)
+  expect(response.data.posts[0].published).toBe(true)
+})
